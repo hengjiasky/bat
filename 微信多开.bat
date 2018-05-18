aec39cdb850ff5fb395d54234dbe006d8e1ceb77
hengjiasky <413067135@qq.com>
2018-05-18 09:11:25 +0800

    first updata

---
+++ 微信多开.bat
@@ -0,0 +1,5 @@
+TASKKILL /F /IM wechat.exe
+start "" "E:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "E:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "E:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "E:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
